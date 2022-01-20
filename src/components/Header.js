376bc742bb5b36f80ef84aa0bf74b14944ed7214
navidabdi <artsanaat@gmail.com>
2022-01-20 17:57:46 +0330

    Add State To The Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useStateContext } from '../contexts/StateCounter';
+
 const Header = () => {
+  const { counter, setCounter } = useStateContext();
   return (
     <StyledHeader>
       <h1>Header</h1>
+      <div className="counter-num">
+        <p>Counter Number:</p>
+        <span>{counter}</span>
+      </div>
     </StyledHeader>
   );
 };
@@ -13,7 +20,13 @@ export default Header;
 const StyledHeader = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   min-height: 10vh;
   border-bottom: 1px solid gray;
+  .counter-num {
+    display: flex;
+    span {
+      margin-left: 0.5rem;
+    }
+  }
 `;
